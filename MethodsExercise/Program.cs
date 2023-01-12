e988bf18ffa1af848f14c92d101230f1c09c219e
Jon Claxton <speedyclax@gmail.com>
2023-01-12 15:13:50 -0600

    exercise one completed

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}. Where are you from?");
+            var location = Console.ReadLine();
+            Console.WriteLine($"{location}? Interesting. What's the temperature today?");
+            var temp = Console.ReadLine();
+            //Wanting to insert if/then statement with a "sounds nice" or "sounds cold" answer dependent on the answer
+            Console.WriteLine($"{temp} sounds nice. Enjoy that weather and have a good day!");
+
+
         }
+
+
+
     }
 }
