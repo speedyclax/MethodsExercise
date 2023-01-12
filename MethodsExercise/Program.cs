dc86a261e731dc3fdabeedf5080d872475b6abc7
Jon Claxton <speedyclax@gmail.com>
2023-01-12 15:44:38 -0600

    overwriting exercise 1 with new work

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,15 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Hello, {name}. Where are you from?");
-            var location = Console.ReadLine();
-            Console.WriteLine($"{location}? Interesting. What's the temperature today?");
-            var temp = Console.ReadLine();
-            //Wanting to insert if/then statement with a "sounds nice" or "sounds cold" answer dependent on the answer
-            Console.WriteLine($"{temp} sounds nice. Enjoy that weather and have a good day!");
+        //----------------------Exercise 1---------------------------------------
+        //Name: 
+        //Favorite Color: 
+        //Favorite Animal: 
+        //Favorite Band: 
 
+            Console.WriteLine("Thank you for signing up for this dating service. First question: What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userName}. Such a lovely name. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color}? That's my favorite color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"{animal}? Really? What are the odds? That's also my favorite animal. Weird. So what is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Get out! I love {band}! We have so much in common. Anyways, here is your basic profile:");
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"Name:            {userName}");
+            Console.WriteLine($"Favorite color:  {color}");
+            Console.WriteLine($"Favorite animal: {animal}");
+            Console.WriteLine($"Favorite band:   {band}");
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.WriteLine("This is awkward, but I really like you. Can I get your number? You can just type it in.");
+
+
+        //----------------------Exercise 2---------------------------------------
 
         }
 
